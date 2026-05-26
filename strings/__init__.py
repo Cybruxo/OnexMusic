355cbfaf1b2e40e40c3ef5754ca34dcc8c36b3c4
𝐒𝐀𝐆𝐐𝐗 ®™ <176986778+Cybruxo@users.noreply.github.com>
2026-05-26 19:52:26 +0530

    Add files via upload

---
+++ strings/__init__.py
@@ -0,0 +1,49 @@
+# * ● OnexMusic
+# * ○ A high-performance engine for streaming music in Telegram voicechats.
+# *
+# * Copyright (C) 2026 Cybruxo
+# *
+# * This program is free software: you can redistribute it and/or modify it under the
+# * terms of the GNU General Public License as published by the Free Software Foundation,
+# * either version 3 of the License, or (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# *
+# * Repository: https://github.com/Cybruxo/OnexMusic
+
+import os
+from typing import List
+
+import yaml
+
+languages = {}
+languages_present = {}
+
+
+def get_string(lang: str):
+    return languages[lang]
+
+
+for filename in os.listdir(r"./strings/langs/"):
+    if "en" not in languages:
+        languages["en"] = yaml.safe_load(
+            open(r"./strings/langs/en.yml", encoding="utf8")
+        )
+        languages_present["en"] = languages["en"]["name"]
+    if filename.endswith(".yml"):
+        language_name = filename[:-4]
+        if language_name == "en":
+            continue
+        languages[language_name] = yaml.safe_load(
+            open(r"./strings/langs/" + filename, encoding="utf8")
+        )
+        for item in languages["en"]:
+            if item not in languages[language_name]:
+                languages[language_name][item] = languages["en"][item]
+    try:
+        languages_present[language_name] = languages[language_name]["name"]
+    except:
+        print("There is some issue with the language file inside bot.")
+        exit()
