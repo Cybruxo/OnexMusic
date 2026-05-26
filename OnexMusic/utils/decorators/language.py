b18e1e5d55ef33658e90e8972218443c96c56494
𝐒𝐀𝐆𝐐𝐗 ®™ <176986778+Cybruxo@users.noreply.github.com>
2026-05-26 20:09:28 +0530

    Add files via upload

---
+++ OnexMusic/utils/decorators/language.py
@@ -0,0 +1,72 @@
+# * ● OnexMusic
+# * ○ A high-performance engine for streaming music in Telegram voicechats.
+# *
+# * Copyright (C) 2026 Cybruxo
+# *
+# * This program is free software: you can redistribute it and/or modify it under the
+# * terms of the GNU General Public License as published by the Free Software Foundation,
+# * either version 3 of the License, or (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# *
+# * Repository: https://github.com/Cybruxo/OnexMusic
+
+from OnexMusic import app
+from OnexMusic.misc import SUDOERS
+from OnexMusic.utils.database import get_lang, is_maintenance
+from strings import get_string
+from config import SUPPORT_CHAT
+
+def language(mystic):
+    async def wrapper(_, message, **kwargs):
+        if await is_maintenance() is False:
+            if message.from_user.id not in SUDOERS:
+                return await message.reply_text(
+                    text=f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ <a href={SUPPORT_CHAT}>sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ</a> ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                    disable_web_page_preview=True,
+                )
+        try:
+            await message.delete()
+        except:
+            pass
+
+        try:
+            language = await get_lang(message.chat.id)
+            language = get_string(language)
+        except:
+            language = get_string("en")
+        return await mystic(_, message, language)
+
+    return wrapper
+
+
+def languageCB(mystic):
+    async def wrapper(_, CallbackQuery, **kwargs):
+        if await is_maintenance() is False:
+            if CallbackQuery.from_user.id not in SUDOERS:
+                return await CallbackQuery.answer(
+                    f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                    show_alert=True,
+                )
+        try:
+            language = await get_lang(CallbackQuery.message.chat.id)
+            language = get_string(language)
+        except:
+            language = get_string("en")
+        return await mystic(_, CallbackQuery, language)
+
+    return wrapper
+
+
+def LanguageStart(mystic):
+    async def wrapper(_, message, **kwargs):
+        try:
+            language = await get_lang(message.chat.id)
+            language = get_string(language)
+        except:
+            language = get_string("en")
+        return await mystic(_, message, language)
+
+    return wrapper
